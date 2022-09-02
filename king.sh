739c04060424193ffe11caf4712ebd14ae7faad6
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-09-02 15:00:54 +0100

    Update king.sh
    *Fix Script

--- king.sh
+++ king.sh
@@ -1,4 +1,12 @@
-hide_guake
+#!/bin/bash
+
+
+function call_cmd(){
+	xte "str $1"
+	sleep 0.5
+	xte "key Return"
+}
+
 call_cmd "fg"
 call_cmd "echo '[USERNAME]' > king.txt "
 call_cmd "chattr +i king.txt"
